1a807dffb3f639318f5b0237e337441f130f84aa
LuisSic <lantoni24@hotmail.com>
2023-10-10 12:55:45 -0600

    Updates commonlib

--- src/Shape.ts
+++ src/Shape.ts
@@ -134,7 +134,7 @@ export class RectShape implements IShape {
     calculateTruePosition: (shapeData: IShapeBase) => IShapeBase,
     selected: boolean,
     degrees: number,
-    image: {
+    _image: {
       width: number;
       height: number;
     }
@@ -160,8 +160,8 @@ export class RectShape implements IShape {
     let newX = x;
     let newY = y;
     if (degrees) {
-      newX = canvas2D.canvas.height - (height + y);
-      newY = canvas2D.canvas.width - (width + x);
+      newX = canvas2D.canvas.width - (height + y);
+      newY = canvas2D.canvas.height - (width + x);
       const radians = toRadians(degrees);
       // const newX = height + y - image.height;
       // const newY = width + x - image.width;
