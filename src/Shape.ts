eca546f2fc1aa2630a67ee0864fe9613dae959ae
LuisSic <lantoni24@hotmail.com>
2023-10-09 21:05:07 -0600

    Updates commonlib

--- src/Shape.ts
+++ src/Shape.ts
@@ -167,7 +167,7 @@ export class RectShape implements IShape {
       canvas2D.save(); // saves current transformation matrix (state)
       canvas2D.translate(originX, originY);
       canvas2D.rotate(radians);
-      canvas2D.translate(newX, newY);
+      canvas2D.translate(-newX, -newY);
       /* const centerX = image.width;
       const centerY = image.height; */
       /*  const newX =
